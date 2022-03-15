3e906d30f7abddfdfa4fd8689032cb0017513a2f
Do Hoang Viet <hvietdo99@Dos-Mac-mini.local>
2022-03-15 15:24:22 +0700

    First commit

---
+++ pages/_app.js
@@ -0,0 +1,38 @@
+import "../styles/globals.css";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { UseWalletProvider } from "use-wallet";
+import NavBar from "../components/Navbar";
+import Footer from "../components/Footer";
+import "@fontsource/space-grotesk";
+
+const theme = extendTheme({
+  fonts: {
+    heading: "Space Grotesk",
+    body: "Space Grotesk",
+  },
+});
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      {" "}
+      <ChakraProvider theme={theme}>
+        <UseWalletProvider
+          chainId={4}
+          connectors={{
+            walletconnect: {
+              rpcUrl:
+                "https://rinkeby.infura.io/v3/451cf3a860254c10bcf681608c94403b",
+            },
+          }}
+        >
+          <NavBar />
+          <Component {...pageProps} />
+          <Footer />{" "}
+        </UseWalletProvider>
+      </ChakraProvider>
+    </>
+  );
+}
+
+export default MyApp;
